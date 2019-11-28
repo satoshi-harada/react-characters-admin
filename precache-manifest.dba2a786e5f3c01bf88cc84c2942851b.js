d790505abc9c8f448c04e58bf31f8f3d5e33de8f
s_harada <satoshi.harada@trustia.jp>
2019-11-28 15:10:46 +0900

    Updates

---
+++ precache-manifest.dba2a786e5f3c01bf88cc84c2942851b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134d27b657c00f5c794c9d365465f52c",
+    "url": "/react-characters-admin/index.html"
+  },
+  {
+    "revision": "49c25293c18da22ada7a",
+    "url": "/react-characters-admin/static/css/main.89424208.chunk.css"
+  },
+  {
+    "revision": "048644293785678cf9a7",
+    "url": "/react-characters-admin/static/js/2.95ce8e67.chunk.js"
+  },
+  {
+    "revision": "49c25293c18da22ada7a",
+    "url": "/react-characters-admin/static/js/main.fb34725a.chunk.js"
+  },
+  {
+    "revision": "6f72e1eb8a95d852ca18",
+    "url": "/react-characters-admin/static/js/runtime-main.fdef6668.js"
+  }
+]);
